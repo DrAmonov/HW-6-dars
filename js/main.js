30f59855d31618fa349c19d5a54d99c5823907a4
DrAmonov <temurxonamonov@gmail.com>
2022-11-26 19:21:06 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var elDesc = document.querySelector('.desc');
+
+var userName = prompt ('Iltimos ismingizni kiriting!');
+var userMoney = prompt ('Sayohat uchun jamgargan mablagingizni kiriting!');
+var forTicket = 500;
+var forHotel = 250;
+var forTravel = 120;
+var halfDollar = ((forHotel + forTicket) * 11225);
+var halfEuro = (forTravel * 11686);
+var total = (halfDollar + halfEuro);
+console.log (total);
+
+
+
+if (userMoney >= total) {
+    elDesc.textContent= 'Oq yo\l' + ' ' + userName + '!'; 
+}else {
+    elDesc.textContent= userName + ' ' + 'Ozgina sabr qilish kerak bo\'lar ekan!';
+}
